d783b67850fab35ad9affe4972d48d5889826a9e
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-22 18:38:46 +0200

    Udate task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const imageEl = document.querySelector(".gallery__image");
+const galleryCards = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryCards);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(
+      (element) => `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+galleryEl.addEventListener("click", onOpenModalWindow);
+
+function onOpenModalWindow(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  basicLightbox
+    .create(
+      `
+		<img  src="${event.target.dataset.source}">
+	`
+    )
+    .show();
+}
